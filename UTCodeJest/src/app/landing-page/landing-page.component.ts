9da3ad1b76d46993f86b1172619c22488ba3e6d3
tsm98 <sairam.tm@gmail.com>
2023-11-29 09:09:33 -0600

    Last commit hopefully lol

--- UTCodeJest/src/app/landing-page/landing-page.component.ts
+++ UTCodeJest/src/app/landing-page/landing-page.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
 })
-export class LandingPageComponent {}
+export class LandingPageComponent implements OnInit {
+  activeSection = 'onboarding';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(): void {
+    const sections = [
+      'onboarding',
+      'section1',
+      'section2',
+      'section3',
+      'section4',
+    ];
+    let currentSection = 'onboarding';
+
+    for (const section of sections) {
+      const element = document.getElementById(section);
+      if (
+        element &&
+        window.pageYOffset >= element.offsetTop - element.clientHeight / 3
+      ) {
+        currentSection = section;
+      }
+    }
+
+    this.activeSection = currentSection;
+  }
+}
